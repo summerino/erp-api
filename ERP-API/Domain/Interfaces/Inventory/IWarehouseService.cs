620f4bbb5b32bf26e1010584893db3872ceb1c51
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-04-20 15:05:24 +0700

    add master warehouse

---
+++ ERP-API/Domain/Interfaces/Inventory/IWarehouseService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ERP_API.Domain.Entities.Inventory;
+using ERP_API.Domain.Models;
+using Swift.Framework.Model;
+
+namespace ERP_API.Domain.Interfaces.Inventory
+{
+    public interface IWarehouseService : IGeneralService<Warehouse>
+    {
+        DataSourceResult GetData(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort,
+            List<int> category, string search);
+
+        SaveResult Delete(string code, int userId);
+    }
+}
