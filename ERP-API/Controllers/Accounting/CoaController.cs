1d68c6adbbcbb5c6beecd1a538e1513f9b718afb
Juniardi Ibrahim <juniardi@sahassa.co.id>
2021-04-23 19:42:02 +0700

    Update Coa, penambahan parameter filters

--- ERP-API/Controllers/Accounting/CoaController.cs
+++ ERP-API/Controllers/Accounting/CoaController.cs
@@ -43,11 +43,11 @@ namespace ERP_API.Controllers.Accounting
         }
 
         [HttpGet("lists")]
-        public IActionResult GetList(string sorts) 
+        public IActionResult GetList(string filters, string sorts) 
         {
             var data =
                 _coa.GetLists(
-                    null,
+                    JsonConvert.DeserializeObject<List<Filter>>(!string.IsNullOrWhiteSpace(filters) ? filters : "[]"),
                     JsonConvert.DeserializeObject<List<Sort>>(!string.IsNullOrWhiteSpace(sorts) ? sorts : "[]")).Data
                     .ToDynamicList()
                     .Select(x => new
