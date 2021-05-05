9f37c04da1e3c8893a5ae040f798b5eb84ae90e2
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-05-05 21:52:55 +0700

    add debit memo and userrequest.cs

---
+++ ERP-API/Controllers/Purchase/DebitMemoController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+using Microsoft.AspNetCore.Mvc;
+using ERP_API.Domain.Interfaces.Purchase;
+using ERP_API.Domain.Models;
+using ERP_API.Model;
+using Newtonsoft.Json;
+
+namespace ERP_API.Controllers.Purchase
+{
+    [Route("api/v1/debit-memo")]
+    [ApiController]
+    public class DebitMemoController : ControllerBase
+    {
+        private readonly IDebitMemoService _memo;
+
+        public DebitMemoController(IDebitMemoService memo)
+        {
+            _memo = memo;
+        }
+
+        [HttpGet]
+        public IActionResult GetData(string search, string filters, string sorts, int skip, int take)
+        {
+            var data =
+                _memo.GetData(
+                    skip, take,
+                    JsonConvert.DeserializeObject<List<Filter>>(!string.IsNullOrWhiteSpace(filters) ? filters : "[]"),
+                    JsonConvert.DeserializeObject<List<Sort>>(!string.IsNullOrWhiteSpace(sorts) ? sorts : "[]"),
+                    search);
+
+            return Ok(new ApiResponse
+            {
+                RowCount = data.Total,
+                TableData = data.Data.ToDynamicList()
+            });
+        }
+
+        [HttpGet("related-trans")]
+        public IActionResult GetRelatedTransactions(string code)
+        {
+            var data = _memo.GetRelatedTransactions(code);
+
+            return Ok(new ApiResponse
+            {
+                RowCount = data.Count,
+                TableData = data
+            });
+        }
+    }
+}
---
+++ ERP-API/Domain/Entities/Purchase/DebitMemo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ERP_API.Domain.Entities.Core;
+
+namespace ERP_API.Domain.Entities.Purchase
+{
+    [Table("DebitMemo", Schema = Schema.Purchasing)]
+    public class DebitMemo : BaseEntityWithMark
+    {
+        [Key]
+        [StringLength(17)]
+        public string Code { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        public short SrcTrans { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string SupCode { get; set; }
+
+        [StringLength(17)]
+        public string TransCode { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string CurrCode { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Rate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Amount { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Used { get; set; }
+
+        [StringLength(256)]
+        public string Notes { get; set; }
+    }
+
+    public class VwDebitMemo : BaseEntityWithMark
+    {
+        public string Code { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public short SrcTrans { get; set; }
+
+        public string SupCode { get; set; }
+
+        public string TransCode { get; set; }
+
+        public string CurrCode { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal Used { get; set; }
+
+        public string Notes { get; set; }
+
+
+        public string SupName { get; set; }
+
+        public string SrcTransName { get; set; }
+
+        public string Status { get; set; }
+    }
+}
