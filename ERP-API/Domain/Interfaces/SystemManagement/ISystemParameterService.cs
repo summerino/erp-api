9faea5e1ac91a08413c55bca4b73a8e63c49124a
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-05-18 19:09:56 +0700

    - add api system parameter - fix master parameter add uniquecolumnname

---
+++ ERP-API/Domain/Interfaces/SystemManagement/ISystemParameterService.cs
@@ -0,0 +1,10 @@
+﻿using ERP_API.Domain.Entities.SystemManagement;
+using System.Collections.Generic;
+
+namespace ERP_API.Domain.Interfaces.SystemManagement
+{
+    public interface ISystemParameterService : IGeneralService<SystemParameter>
+    {
+        IEnumerable<SystemParameter> GetList(string[] codes);
+    }
+}
