f5b2a260121dfd4f7e4fe9febb3883813d016813
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-05-25 16:22:48 +0700

    Fix success save message not in bahasa indonesia

--- ERP-API/Domain/Services/Purchase/PurchaseReceiveService.cs
+++ ERP-API/Domain/Services/Purchase/PurchaseReceiveService.cs
@@ -182,7 +182,7 @@ namespace ERP_API.Domain.Services.Purchase
 
             result.Success = true;
             result.Data = data.Code;
-            result.Message = "Success insert purchase receive.";
+            result.Message = "Data penerimaan pembelian berhasil disimpan.";
             return result;
         }
 
