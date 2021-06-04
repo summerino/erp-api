ecce7f6b84f44f00d17a1997f741bbd5cbe70685
Rudy Wang <rudywang87@gmail.com>
2021-06-04 14:37:54 +0700

    Adding migrations for alter procedure stock mutations

--- ERP-API/Migrations/TenantMigrations/20210603071634_UpdateViewSalesReturnDetail.cs
+++ ERP-API/Migrations/TenantMigrations/20210603071634_UpdateViewSalesReturnDetail.cs
@@ -7,49 +7,47 @@ namespace ERP_API.Migrations.TenantMigrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
 			var sql = @"ALTER VIEW [Sales].[vwSalesReturnDetail]
-						AS
-							SELECT sr_d.*,
-								i.Initial AS ItemInitial,
-								i.[Name] AS ItemName,
-								i.UomSellId AS ItemUomSellId,
-								uom_c_s.UnitEquivalent AS ItemUomSellName,
-								i.SellPrice AS ItemSellPrice,
-								uom.Initial AS UomInitial,
-								uom_c.UnitEquivalent AS UnitName
-							FROM Sales.SalesReturnDetail sr_d
-							LEFT JOIN Inventory.Item i
-								ON i.Id = sr_d.ItemId
-							LEFT JOIN Inventory.UoMConversion uom_c_s
-								ON uom_c_s.Id = i.UomSellId
-							LEFT JOIN Inventory.UoM uom
-								ON uom.Id = sr_d.UomId
-							LEFT JOIN Inventory.UoMConversion uom_c
-								ON uom_c.Id = sr_d.UnitId
-						GO";
+AS
+	SELECT sr_d.*,
+		i.Initial AS ItemInitial,
+		i.[Name] AS ItemName,
+		i.UomSellId AS ItemUomSellId,
+		uom_c_s.UnitEquivalent AS ItemUomSellName,
+		i.SellPrice AS ItemSellPrice,
+		uom.Initial AS UomInitial,
+		uom_c.UnitEquivalent AS UnitName
+	FROM Sales.SalesReturnDetail sr_d
+	LEFT JOIN Inventory.Item i
+		ON i.Id = sr_d.ItemId
+	LEFT JOIN Inventory.UoMConversion uom_c_s
+		ON uom_c_s.Id = i.UomSellId
+	LEFT JOIN Inventory.UoM uom
+		ON uom.Id = sr_d.UomId
+	LEFT JOIN Inventory.UoMConversion uom_c
+		ON uom_c.Id = sr_d.UnitId";
 			migrationBuilder.Sql(sql);
 		}
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
 			var sql = @"ALTER VIEW [Sales].[vwSalesReturnDetail]
-						AS
-							SELECT sr_d.*,
-								i.[Name] AS ItemName,
-								i.UomSellId AS ItemUomSellId,
-								uom_c_s.UnitEquivalent AS ItemUomSellName,
-								i.SellPrice AS ItemSellPrice,
-								uom.Initial AS UomInitial,
-								uom_c.UnitEquivalent AS UnitName
-							FROM Sales.SalesReturnDetail sr_d
-							LEFT JOIN Inventory.Item i
-								ON i.Id = sr_d.ItemId
-							LEFT JOIN Inventory.UoMConversion uom_c_s
-								ON uom_c_s.Id = i.UomSellId
-							LEFT JOIN Inventory.UoM uom
-								ON uom.Id = sr_d.UomId
-							LEFT JOIN Inventory.UoMConversion uom_c
-								ON uom_c.Id = sr_d.UnitId
-						GO";
+AS
+	SELECT sr_d.*,
+		i.[Name] AS ItemName,
+		i.UomSellId AS ItemUomSellId,
+		uom_c_s.UnitEquivalent AS ItemUomSellName,
+		i.SellPrice AS ItemSellPrice,
+		uom.Initial AS UomInitial,
+		uom_c.UnitEquivalent AS UnitName
+	FROM Sales.SalesReturnDetail sr_d
+	LEFT JOIN Inventory.Item i
+		ON i.Id = sr_d.ItemId
+	LEFT JOIN Inventory.UoMConversion uom_c_s
+		ON uom_c_s.Id = i.UomSellId
+	LEFT JOIN Inventory.UoM uom
+		ON uom.Id = sr_d.UomId
+	LEFT JOIN Inventory.UoMConversion uom_c
+		ON uom_c.Id = sr_d.UnitId";
 			migrationBuilder.Sql(sql);
 		}
     }
