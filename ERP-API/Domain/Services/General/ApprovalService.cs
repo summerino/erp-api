477391aaa4359c0de0488a3ed017530c1fa0cea7
Wildan Nugraha <wildan.nugraha@sahassa.co.id>
2021-06-30 18:18:51 +0700

    refactor

--- ERP-API/Domain/Services/General/ApprovalService.cs
+++ ERP-API/Domain/Services/General/ApprovalService.cs
@@ -1,10 +1,11 @@
 ﻿using ERP_API.Domain.Entities;
+using ERP_API.Domain.Entities.Core;
 using ERP_API.Domain.Extensions;
 using ERP_API.Domain.Interfaces.General;
 using ERP_API.Domain.Models;
-using ERP_API.Model;
 using ERP_API.Model.General;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,7 +27,7 @@ namespace ERP_API.Domain.Services.General
             if (!string.IsNullOrEmpty(search))
             {
                 data = data.Where(x =>
-                        x.Code.Contains(search) || x.Name.Contains(search) || x.Type.Contains(search));
+                        x.Code.Contains(search) || x.Name.Contains(search) || x.SourceTrans.Contains(search));
             }
 
             return data.ToDataSourceResult(skip, take, filter, sort);
@@ -38,32 +39,37 @@ namespace ERP_API.Domain.Services.General
             var listQuery = new List<string>();
             foreach (var item in data)
             {
-                listQuery.Add(GenerateQuery(item.Type, item.Code, userId));
+                listQuery.Add(GenerateQuery(item.SourceTrans, item.Code, userId));
             }
+
             var query = string.Join(';',listQuery);
-            using (var context = _tenantCtx)
-            using (var command = context.Database.GetDbConnection().CreateCommand())
+            using var transaction = _tenantCtx.Database.BeginTransaction();
+            try
             {
-                command.CommandText = query;
-                context.Database.OpenConnection();
-                command.ExecuteNonQuery();
+                _tenantCtx.Database.ExecuteSqlRaw(query);
+                transaction.Commit();
             }
+            catch (Exception ex) 
+            {
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+                transaction.Rollback();
+            }
+
             result.Success = true;
             result.Message = "Data berhasil di approve.";
             return result;
         }
 
-        private string GenerateQuery(string type, string code, int user) 
+        private string GenerateQuery(string sourceTrans, string code, int user) 
         {
             var map = new MapApproval();
             string tableName = "";
             string query = "Update [TABLE] set ApprovedBy='[USER]', ApprovedDate=GETDATE() Where Code = '[CODE]'";
-            var temp = map.Approvals.SingleOrDefault(x => x.Description.Equals(type));
+            var temp = map.Approvals.SingleOrDefault(x => x.Description.Equals(sourceTrans));
             if (temp != null) 
             {
                 tableName = temp.TableName;
             }
-            
             return query.Replace("[TABLE]", tableName).Replace("[CODE]", code).Replace("[USER]", user.ToString());
         }
     }
