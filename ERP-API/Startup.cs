076658b89e0dd49e89c1974b32c756af01e3c187
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2021-07-12 15:43:55 +0700

    Remove un-used code in ShardingService

--- ERP-API/Startup.cs
+++ ERP-API/Startup.cs
@@ -103,30 +103,31 @@ namespace ERP.Web.API
             });
 
             // Add authentication service
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(jwt => {
-                var token = Configuration.GetSection("JwtConfig").Get<JwtConfig>();
-
-                jwt.RequireHttpsMetadata = false;
-                jwt.ClaimsIssuer = token.Issuer;
-                jwt.SaveToken = true;
-                jwt.TokenValidationParameters = new TokenValidationParameters
+            services
+                .AddAuthentication(options =>
                 {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(token.Secret)),
-                    ValidIssuer = token.Issuer,
-                    ValidateIssuer = true,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    RequireExpirationTime = true,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(jwt => {
+                    var token = Configuration.GetSection("JwtConfig").Get<JwtConfig>();
+
+                    jwt.RequireHttpsMetadata = false;
+                    jwt.ClaimsIssuer = token.Issuer;
+                    jwt.SaveToken = true;
+                    jwt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(token.Secret)),
+                        ValidIssuer = token.Issuer,
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        RequireExpirationTime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
 
             // Add application service
             // Core services
@@ -134,16 +135,16 @@ namespace ERP.Web.API
             services.AddScoped<IShardingService, ShardingService>();
             services.AddScoped<IClaimService, ClaimService>();
             services.AddScoped<IAuthService, AuthService>();
-            
+
             // Accounting services
-            services.AddScoped<ICoaService, CoaService>();
-            services.AddScoped<ICurrencyRateService, CurrencyRateService>();
-            services.AddScoped<ICoaTypeService, CoaTypeService>();
             services.AddScoped<IAccountPayableService, AccountPayableService>();
             services.AddScoped<IAccountReceivableService, AccountReceivableService>();
+            services.AddScoped<ICoaService, CoaService>();
+            services.AddScoped<ICoaTypeService, CoaTypeService>();
+            services.AddScoped<ICurrencyRateService, CurrencyRateService>();
             services.AddScoped<IGeneralJournalService, GeneralJournalService>();
 
-            // Asset services
+            // Asset Management services
             services.AddScoped<IAssetTypeService, AssetTypeService>();
             services.AddScoped<IFixedAssetService, FixedAssetService>();
 
@@ -155,29 +156,28 @@ namespace ERP.Web.API
             services.AddScoped<ICashBankTypeService, CashBankTypeService>();
 
             // General services
+            services.AddScoped<IApprovalService, ApprovalService>();
             services.AddScoped<ICurrencyService, CurrencyService>();
             services.AddScoped<ICustomerService, CustomerService>();
             services.AddScoped<ICustomerTypeService, CustomerTypeService>();
             services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IPaymentTermService, PaymentTermService>();
             services.AddScoped<ISupplierService, SupplierService>();
             services.AddScoped<ISupplierTypeService, SupplierTypeService>();
             services.AddScoped<ITaxService, TaxService>();
             services.AddScoped<IVehicleService, VehicleService>();
             services.AddScoped<IVehicleTypeService, VehicleTypeService>();
-            services.AddScoped<ICurrencyService, CurrencyService>();
-            services.AddScoped<IItemGroupService, ItemGroupService>();
-            services.AddScoped<IPaymentTermService, PaymentTermService>();
-            services.AddScoped<IApprovalService, ApprovalService>();
 
             // Inventory services
             services.AddScoped<IAdjustmentService, AdjustmentService>();
             services.AddScoped<IItemCategoryService, ItemCategoryService>();
+            services.AddScoped<IItemGroupService, ItemGroupService>();
             services.AddScoped<IItemService, ItemService>();
             services.AddScoped<ITransferStockService, TransferStockService>();
             services.AddScoped<IUnitOfMeasurementService, UnitOfMeasurementService>();
             services.AddScoped<IUoMConversionService, UoMConversionService>();
-            services.AddScoped<IWarehouseService, WarehouseService>();
             services.AddScoped<IWarehouseQuantityService, WarehouseQuantityService>();
+            services.AddScoped<IWarehouseService, WarehouseService>();
 
             // Purchase services
             services.AddScoped<IDebitMemoService, DebitMemoService>();
@@ -187,31 +187,30 @@ namespace ERP.Web.API
             services.AddScoped<IPurchaseReturnService, PurchaseReturnService>();
 
             // Sales services
+            services.AddScoped<IAreaService, AreaService>();
             services.AddScoped<ICreditMemoService, CreditMemoService>();
+            services.AddScoped<IDeliveryPlanService, DeliveryPlanService>();
             services.AddScoped<IDirectInvoiceService, DirectInvoiceService>();
+            services.AddScoped<IPromoService, PromoService>();
             services.AddScoped<ISalesDeliveryService, SalesDeliveryService>();
             services.AddScoped<ISalesInvoiceService, SalesInvoiceService>();
+            services.AddScoped<ISalesmanGroupService, SalesmanGroupService>();
+            services.AddScoped<ISalesmanService, SalesmanService>();
             services.AddScoped<ISalesOrderService, SalesOrderService>();
             services.AddScoped<ISalesReturnService, SalesReturnService>();
-            services.AddScoped<IAreaService, AreaService>();
-            services.AddScoped<ISalesReturnService, SalesReturnService>();
-            services.AddScoped<IDeliveryPlanService, DeliveryPlanService>();
-            services.AddScoped<IPromoService, PromoService>();
-            services.AddScoped<IVisitPlanService, VisitPlanService>();
             services.AddScoped<IVisitOrderService, VisitOrderService>();
-            services.AddScoped<ISalesmanService, SalesmanService>();
-            services.AddScoped<ISalesmanGroupService, SalesmanGroupService>();
+            services.AddScoped<IVisitPlanService, VisitPlanService>();
 
             // System Management services
+            services.AddScoped<ICompanyProfileService, CompanyProfileService>();
             services.AddScoped<IMenuService, MenuService>();
             services.AddScoped<IRoleService, RoleService>();
             services.AddScoped<ISystemParameterService, SystemParameterService>();
             services.AddScoped<IUserService, UserService>();
-            services.AddScoped<ICompanyProfileService, CompanyProfileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CatalogContext controlDbContext,
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CatalogContext catalogCtx,
             IShardingService shardingService, IServiceProvider service­Provider)
         {
             //app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -235,18 +234,13 @@ namespace ERP.Web.API
                 endpoints.MapControllers().RequireAuthorization();
             });
 
-            EnsureDatabaseCreated(controlDbContext, shardingService);
+            EnsureDatabaseCreated(catalogCtx, shardingService);
             Builder.InitConfiguration(ControlDbConnectionString);
         }
 
-        public virtual void EnsureDatabaseCreated(CatalogContext controlDbContext,
-            IShardingService shardingService)
+        public virtual void EnsureDatabaseCreated(CatalogContext catalogCtx, IShardingService shardingService)
         {
-            //if (!DatabaseUtility.DatabaseExists(ControlDbConnectionString))
-            //{
-            //    DatabaseUtility.CreateDatabase(ControlDbConnectionString);
-            //}
-            controlDbContext.Database.Migrate();
+            catalogCtx.Database.Migrate();
             shardingService.ApplyMigrationAsync().GetAwaiter().GetResult();
         }
     }
