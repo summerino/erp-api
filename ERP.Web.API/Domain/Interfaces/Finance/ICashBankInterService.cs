0a1bb6c9cf4cb00a344d84f0b644fa377c2a288c
Juniardi Ibrahim <juniardi@sahassa.co.id>
2021-07-13 13:24:07 +0700

    Add Inter Cash Bank Backend

---
+++ ERP.Web.API/Domain/Interfaces/Finance/ICashBankInterService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ERP.Common;
+using ERP.Common.Models;
+using ERP.Entity.Finance;
+using ERP.Web.API.Model.Finance;
+
+namespace ERP.Web.API.Domain.Interfaces.Finance
+{
+    public interface ICashBankInterService : IGeneralService<GeneralCashBankHeader>
+    {
+        DataSourceResult GetData(int skip, int take, IEnumerable<Filter> filters, IEnumerable<Sort> sorts,
+            string search);
+
+        IEnumerable<VwGeneralCashBankDetail> GetDetailData(string code);
+
+        SaveResult Insert(CashBankInterRequest data);
+
+        SaveResult Update(CashBankInterRequest data);
+
+        SaveResult Delete(string code, int userId);
+    }
+}
---
+++ ERP.Web.API/Model/Finance/CashBankInterRequest.cs
@@ -0,0 +1,10 @@
+﻿using ERP.Entity.Finance;
+using System.Collections.Generic;
+
+namespace ERP.Web.API.Model.Finance
+{
+    public class CashBankInterRequest : GeneralCashBankHeader
+    {
+        public IEnumerable<GeneralCashBankDetail> ItemDetails { get; set; }
+    }
+}
