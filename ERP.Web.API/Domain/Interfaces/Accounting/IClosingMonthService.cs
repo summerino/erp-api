0bc394f95f6034bcfa2d486b1145bf50f4c0c1d7
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-07-16 18:45:02 +0700

    Adding Beginning Balance Credit Memo Backend

---
+++ ERP.Web.API/Domain/Interfaces/Accounting/IClosingMonthService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ERP.Common;
+using ERP.Common.Models;
+using ERP.Entity.Accounting;
+using ERP.Web.API.Model.Accounting;
+
+namespace ERP.Web.API.Domain.Interfaces.Accounting
+{
+    public interface IClosingMonthService : IGeneralService<ClosingMonth>
+    {
+        DataSourceResult GetData(int skip, int take, IEnumerable<Filter> filters, IEnumerable<Sort> sorts,
+            string search);
+        
+        SaveResult Insert(ClosingMonthRequest data);
+
+    }
+}
