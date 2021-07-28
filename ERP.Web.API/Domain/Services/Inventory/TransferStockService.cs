e627ccb19e37ea99fa6cb0d5c4a2b2615ac48efb
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2021-07-28 11:53:38 +0700

    - Fixing bugs set mark OriginTransferCode - Simplify & clean up code
    Related Work Item: #4148

--- ERP.Web.API/Domain/Services/Inventory/TransferStockService.cs
+++ ERP.Web.API/Domain/Services/Inventory/TransferStockService.cs
@@ -76,9 +76,9 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 foreach (var item in data.ItemDetails)
                 {
                     // Checking qty
-                    if (item.Qty < 0)
+                    if (item.Qty <= 0)
                     {
-                        result.Message = "Qty tidak boleh negative.";
+                        result.Message = "Qty harus lebih besar dari nol.";
                         return result;
                     }
 
@@ -118,50 +118,46 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 }
 
                 // Summary
-                string errorList = "";
-                string itemName = "";
-                if (existListItemId.Count > 0 && existListItemId != null)
+                var errorList = "";
+                string itemName;
+                if (existListItemId.Count > 0)
                 {
-                    for (int j = 0; j < existListItemId.Count; j++)
+                    foreach (var itemId in existListItemId)
                     {
-                        itemName = items.Find(x => x.Id == existListItemId[j]).Name;
-                        if (errorList == "")
-                        {
-                            errorList = "&bull; Barang " + itemName + " tidak tersedia.";
-                        }
-                        else
-                        {
-                            errorList += "<br/>&bull; Barang " + itemName + " tidak tersedia.";
-                        }
+                        itemName = items.Find(x => x.Id == itemId)?.Name;
+                        errorList += $"<br/>&bull; Barang {itemName} tidak tersedia.";
                     }
                 }
 
-                if (qtyListItemId.Count > 0 && qtyListItemId != null)
+                if (qtyListItemId.Count > 0)
                 {
-                    for (int j = 0; j < qtyListItemId.Count; j++)
+                    foreach (var itemId in qtyListItemId)
                     {
-                        itemName = items.Find(x => x.Id == qtyListItemId[j]).Name;
-                        if (errorList == "")
-                        {
-                            errorList = "&bull; Qty barang " + itemName + " pada gudang asal tidak mencukupi.";
-                        }
-                        else
-                        {
-                            errorList += "<br/>&bull; Qty barang " + itemName + " pada gudang asal tidak mencukupi.";
-                        }
+                        itemName = items.Find(x => x.Id == itemId)?.Name;
+                        errorList += $"<br/>&bull; Qty barang {itemName} pada gudang asal tidak mencukupi.";
                     }
                 }
 
-                if (errorList != "")
+                if (!string.IsNullOrEmpty(errorList))
                 {
+                    errorList = errorList[5..];
+
                     result.Message = errorList;
                     return result;
                 }
 
                 Db.SaveChanges();
 
+                // Update origin transfer code mark to CMP
+                if (data.Type == "IN")
+                {
+                    Db.Database.ExecuteSqlRaw(
+                        "UPDATE Inventory.TransferStockHeader SET Mark='CMP' WHERE Code={0} AND Mark!='V'",
+                        data.OriginTransferCode);
+                }
+
                 // Execute sp_update_transfer_stock
-                Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0},{1},{2}", 
+                Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0}, {1}, {2}", 
                     data.Code, data.Date, 0);
 
                 transaction.Commit();
@@ -174,7 +170,7 @@ namespace ERP.Web.API.Domain.Services.Inventory
 
             result.Success = true;
             result.Data = data.Code;
-            result.Message = "Data transfer stok berhasil disimpan.";
+            result.Message = "Data transfer persediaan berhasil disimpan.";
             return result;
         }
 
@@ -190,7 +186,7 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 // Checking mark header data
                 if (Db.TransferStockHeaders.Any(x => x.Code == data.Code && x.Mark == "V"))
                 {
-                    result.Message = "Data transfer stok tidak bisa diubah karena sudah ditandai sebagai void.";
+                    result.Message = "Data transfer persediaan tidak bisa diubah karena sudah ditandai sebagai void.";
                     return result;
                 }
 
@@ -204,8 +200,7 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 Db.Entry(data).Property(e => e.CreatedDate).IsModified = false;
 
                 // Get detail data that exists in order before
-                var delDetails = Db.TransferStockDetails
-                    .Where(d => d.Code == data.Code).ToList();
+                var delDetails = Db.TransferStockDetails.Where(d => d.Code == data.Code).ToList();
 
                 // Delete detail data that exists in order before
                 Db.TransferStockDetails.RemoveRange(delDetails);
@@ -217,9 +212,9 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 foreach (var item in data.ItemDetails)
                 {
                     // Checking qty
-                    if (item.Qty < 0)
+                    if (item.Qty <= 0)
                     {
-                        result.Message = "Qty tidak boleh negative.";
+                        result.Message = "Qty harus lebih besar dari nol.";
                         return result;
                     }
 
@@ -259,50 +254,62 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 }
 
                 // Summary
-                string errorList = "";
-                string itemName = "";
-                if (existListItemId.Count > 0 && existListItemId != null)
+                var errorList = "";
+                string itemName;
+                if (existListItemId.Count > 0)
                 {
-                    for (int j = 0; j < existListItemId.Count; j++)
+                    foreach (var itemId in existListItemId)
                     {
-                        itemName = items.Find(x => x.Id == existListItemId[j]).Name;
-                        if (errorList == "")
-                        {
-                            errorList = "&bull; Barang " + itemName + " tidak tersedia.";
-                        }
-                        else
-                        {
-                            errorList += "<br/>&bull; Barang " + itemName + " tidak tersedia.";
-                        }
+                        itemName = items.Find(x => x.Id == itemId)?.Name;
+                        errorList += $"<br/>&bull; Barang {itemName} tidak tersedia.";
                     }
                 }
 
-                if (qtyListItemId.Count > 0 && qtyListItemId != null)
+                if (qtyListItemId.Count > 0)
                 {
-                    for (int j = 0; j < qtyListItemId.Count; j++)
+                    foreach (var itemId in qtyListItemId)
                     {
-                        itemName = items.Find(x => x.Id == qtyListItemId[j]).Name;
-                        if (errorList == "")
-                        {
-                            errorList = "&bull; Qty barang " + itemName + " pada gudang asal tidak mencukupi.";
-                        }
-                        else
-                        {
-                            errorList += "<br/>&bull; Qty barang " + itemName + " pada gudang asal tidak mencukupi.";
-                        }
+                        itemName = items.Find(x => x.Id == itemId)?.Name;
+                        errorList += $"<br/>&bull; Qty barang {itemName} pada gudang asal tidak mencukupi.";
                     }
                 }
 
-                if (errorList != "")
+                if (!string.IsNullOrEmpty(errorList))
                 {
+                    errorList = errorList[5..];
+
                     result.Message = errorList;
                     return result;
                 }
 
+                // Update origin transfer code mark to A first
+                if (data.Type == "IN")
+                {
+                    Db.Database.ExecuteSqlRaw(
+                        @"UPDATE TBA
+                        SET TBA.Mark = 'A'
+                        FROM Inventory.TransferStockHeader TBA
+                        WHERE EXISTS (
+                            SELECT OriginTransferCode 
+                            FROM Inventory.TransferStockHeader TBB
+                            WHERE TBB.Code = {0}
+                            AND TBB.OriginTransferCode = TBA.Code
+                        )
+                        AND TBA.Mark != 'V'", data.Code);
+                }
+
                 Db.SaveChanges();
 
+                // Update origin transfer code mark to CMP
+                if (data.Type == "IN")
+                {
+                    Db.Database.ExecuteSqlRaw(
+                        "UPDATE Inventory.TransferStockHeader SET Mark='CMP' WHERE Code={0} AND Mark!='V'",
+                        data.OriginTransferCode);
+                }
+
                 // Execute sp_update_transfer_stock
-                Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0},{1},{2}",
+                Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0}, {1}, {2}",
                     data.Code, data.Date, 0);
 
                 transaction.Commit();
@@ -315,7 +322,7 @@ namespace ERP.Web.API.Domain.Services.Inventory
 
             result.Success = true;
             result.Data = data.Code;
-            result.Message = "Data transfer stok berhasil diperbarui.";
+            result.Message = "Data transfer persediaan berhasil diperbarui.";
             return result;
         }
 
@@ -329,31 +336,50 @@ namespace ERP.Web.API.Domain.Services.Inventory
                 // Checking mark header data
                 if (data.Mark == "V")
                 {
-                    result.Message = "Data transfer stok tidak bisa ditandai sebagai void karena sudah ditandai sebagai void.";
+                    result.Message = "Data transfer persediaan tidak bisa ditandai sebagai void karena sudah ditandai sebagai void.";
                     return result;
                 }
 
                 // Checking void ordered for type 1 only
                 if (data.Type == "OUT" && !IsInventoryInAlreadyVoid(data.Code))
                 {
-                    result.Message = "Data transfer stok tidak bisa ditandai sebagai void karena transfer persediaan pada barang masuk masih aktif.";
+                    result.Message = "Data transfer persediaan tidak bisa ditandai sebagai void karena transfer persediaan pada barang masuk masih aktif.";
                     return result;
                 }
 
-                // Update header data
-                data.Mark = "V";
-                data.UpdatedBy = userId;
-                data.UpdatedDate = DateTime.Now;
+                using var transaction = Db.Database.BeginTransaction();
+                try
+                {
+                    // Update header data
+                    data.Mark = "V";
+                    data.UpdatedBy = userId;
+                    data.UpdatedDate = DateTime.Now;
 
-                Db.SaveChanges();
+                    Db.SaveChanges();
 
-                // Execute sp_update_transfer_stock
-                Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0},{1},{2}",
-                    data.Code, data.Date, 1);
+                    // Update origin transfer code mark to A
+                    if (data.Type == "IN")
+                    {
+                        Db.Database.ExecuteSqlRaw(
+                            "UPDATE Inventory.TransferStockHeader SET Mark='A' WHERE Code={0} AND Mark!='V'",
+                            data.OriginTransferCode);
+                    }
+
+                    // Execute sp_update_transfer_stock
+                    Db.Database.ExecuteSqlRaw("EXEC sp_update_transfer_stock {0}, {1}, {2}",
+                        data.Code, data.Date, 1);
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    result.Message = ex.InnerException?.Message ?? ex.Message;
+                    return result;
+                }
             }
 
             result.Success = true;
-            result.Message = "Data transfer stok berhasil ditandai sebagai void.";
+            result.Message = "Data transfer persediaan berhasil ditandai sebagai void.";
             return result;
         }
 
@@ -369,61 +395,27 @@ namespace ERP.Web.API.Domain.Services.Inventory
 
         private bool IsWarehouseItemQtyExists(string warehouseCode, int itemId, int uomId, int unitId, decimal qty)
         {
-            decimal val = 1;
             var m = Db.UoMConversions.Where(x => x.UomId == uomId).OrderBy(x => x.Seq).ToList();
 
             var unit = m.Find(x => x.Id == unitId);
 
-            var data = Db.WarehouseQuantities.Where(x => x.WarehouseCode == warehouseCode && x.ItemId == itemId).ToList();
+            var data = Db.WarehouseQuantities.SingleOrDefault(x => x.WarehouseCode == warehouseCode && x.ItemId == itemId);
+
+            if (data == null)
+                return true;
 
             if (unit.IsBaseUnit)
             {
-                if (data.Count > 0)
-                {
-                    decimal qtyAvailable = data[0].QtyOnHand - data[0].QtyOnOrder;
-                    if (qtyAvailable < qty)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    return true;
-                }
+                return data.QtyOnHand - data.QtyOnOrder >= qty;
             }
-            else
-            {
-                for (int i = 0; i < m.Count; i++)
-                {
-                    if (m[i].Seq <= unit.Seq)
-                    {
-                        val *= m[i].Conversion;
-                    }
-                }
 
-                if (data.Count > 0)
-                {
-                    decimal qtyAvailable = data[0].QtyOnHand - data[0].QtyOnOrder;
-                    decimal itemConverted = qtyAvailable / val;
+            var val = 
+                m.Where(x => x.Seq <= unit.Seq)
+                    .Aggregate<UoMConversion, decimal>(1, (current, x) => current * x.Conversion);
 
-                    if (itemConverted < qty)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                    return true;
-                }
-            }
+            var itemConverted = (data.QtyOnHand - data.QtyOnOrder) / val;
+
+            return itemConverted >= qty;
         }
     }
 }
