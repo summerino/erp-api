e32aeb7ea030cace21f026cde61f79ce8d91a871
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-08-12 17:28:06 +0700

    Adding Report Cash Bank BackEnd

---
+++ ERP.Entity/Finance/CBReport.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ERP.Entity.Finance
+{
+    public class ReportByAllAccount
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BeginningBalance { get; set; }
+
+        public decimal IncomingBalance { get; set; }
+
+        public decimal OutgoingBalance { get; set; }
+
+        public decimal EndingBalance { get; set; }
+    }
+
+    public class ReportByAccount
+    {
+        public DateTime? Date { get; set; }
+
+        public string Code { get; set; }
+
+        public string Notes { get; set; }
+
+        public decimal? IncomingBalance { get; set; }
+
+        public decimal? OutgoingBalance { get; set; }
+
+        public decimal EndingBalance { get; set; }
+
+        public bool IsBold { get; set; }
+
+    }
+
+    public class ReportByAccountDetail
+    {
+        public DateTime? Date { get; set; }
+
+        public string Code { get; set; }
+
+        public string Notes { get; set; }
+
+        public string TransCode { get; set; }
+
+        public string CoaCode { get; set; }
+
+        public string CoaName { get; set; }
+
+        public decimal? IncomingBalance { get; set; }
+
+        public decimal? OutgoingBalance { get; set; }
+
+        public decimal EndingBalance { get; set; }
+
+        public bool IsBold { get; set; }
+    }
+}
---
+++ ERP.Web.API/Controllers/Finance/CBReportController.cs
@@ -0,0 +1,32 @@
+﻿using ERP.Common.Models;
+using ERP.Web.API.Domain.Interfaces.Finance;
+using ERP.Web.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+
+namespace ERP.Web.API.Controllers.Finance
+{
+    [Route("cb-report")]
+    [ApiController]
+    public class CBReportController : ControllerBase
+    {
+        private readonly ICBReportService _cb;
+        public CBReportController(ICBReportService cb)
+        {
+            _cb = cb;
+        }
+        [HttpGet]
+        public IActionResult GetData(int? type, string startDate, string endDate, string coaCode, string sorts)
+        {
+            var result = _cb.GetData(type, startDate, endDate, coaCode, JsonConvert.DeserializeObject<List<Sort>>(!string.IsNullOrWhiteSpace(sorts) ? sorts : "[]"));
+
+            return Ok(new ApiResponse
+            {
+                RowCount = result.Total,
+                TableData = result.Data.ToDynamicList()
+            });
+        }
+    }
+}
