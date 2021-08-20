e9ef7d3eb537ea4cb6b0685bdd61b1a577d5f7a0
wildan nugraha <wildan.nugraha@sahassa.co.id>
2021-08-20 12:56:42 +0700

    Adding Mobile Signin Element In Master Customer

--- ERP.Web.API/Model/General/CustomerRequest.cs
+++ ERP.Web.API/Model/General/CustomerRequest.cs
@@ -5,6 +5,7 @@ namespace ERP.Web.API.Model.General
 {
     public class CustomerRequest : Customer
     {
+        public string MobilePassword { get; set; }
         public IEnumerable<CustomerAddress> ItemDetails { get; set; }
     }
 }
