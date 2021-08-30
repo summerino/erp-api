483b931cc3545c88d673a5fdbb81c1479d16607c
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-08-30 10:32:39 +0700

    Fix report AP & AR query

--- ERP.Entity/Purchase/AccountPayableReport.cs
+++ ERP.Entity/Purchase/AccountPayableReport.cs
@@ -23,7 +23,7 @@ namespace ERP.Entity.Purchase
     {
         public DateTime Date { get; set; }
 
-        public DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
 
         public string Code { get; set; }
 
