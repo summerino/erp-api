0fb804bb5012fbf8cc36fc37d2d291fd22e40aa6
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-08-30 17:15:21 +0700

    Adding Report Trial Balance BackEnd

---
+++ ERP.Web.API/Domain/Interfaces/Accounting/ITrialBalanceReportService.cs
@@ -0,0 +1,13 @@
+﻿using ERP.Entity.Accounting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Web.API.Domain.Interfaces.Accounting
+{
+    public interface ITrialBalanceReportService
+    {
+        IEnumerable<TrialBalanceResult> GetTrialBalanceLists(string rptBy, string dateFrom, string dateTo, string currCode);
+    }
+}
