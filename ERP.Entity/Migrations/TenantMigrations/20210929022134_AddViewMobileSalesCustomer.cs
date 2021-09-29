81f108bd455f25113fa9a8eb08d7dacb85d87d25
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2021-09-29 09:25:32 +0700

    Adding migration create view mobile customer

---
+++ ERP.Entity/Migrations/TenantMigrations/20210929022134_AddViewMobileSalesCustomer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERP.Entity.Migrations.TenantMigrations
+{
+    public partial class AddViewMobileSalesCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"CREATE VIEW [MobileSales].[vwMobileCustomer]
+AS
+    SELECT mc.*,
+        u_c.Initial AS CreatedInitial,
+        u_u.Initial AS UpdatedInitial,
+        u_a.Initial AS ApprovedInitial,
+        u_r.Initial AS RejectedInitial,
+        CASE mc.Mark
+            WHEN 'A' THEN 'Active'
+            WHEN 'APR' THEN 'Approved'
+            WHEN 'REJ' THEN 'Rejected' END AS [Status]
+    FROM MobileSales.MobileCustomer mc
+    LEFT JOIN SystemManagement.[User] u_c
+        ON u_c.Id = mc.CreatedBy
+    LEFT JOIN SystemManagement.[User] u_u
+        ON u_u.Id = mc.UpdatedBy
+    LEFT JOIN SystemManagement.[User] u_a
+        ON u_a.Id = mc.ApprovedBy
+	LEFT JOIN SystemManagement.[User] u_r
+        ON u_r.Id = mc.RejectedBy";
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"DROP VIEW [MobileSales].[vwMobileCustomer]";
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
