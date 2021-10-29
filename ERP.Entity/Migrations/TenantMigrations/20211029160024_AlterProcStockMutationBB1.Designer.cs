f8554407928ef76e8169d9f003bdd740c67a784e
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2021-10-29 23:03:05 +0700

    update proc stock mutation bb

--- ERP.Entity/Migrations/TenantMigrations/20211029160024_AlterProcStockMutationBB1.Designer.cs
+++ ERP.Entity/Migrations/TenantMigrations/20211029160024_AlterProcStockMutationBB1.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERP.Entity.Migrations.TenantMigrations
 {
     [DbContext(typeof(TenantContext))]
-    partial class TenantContextModelSnapshot : ModelSnapshot
+    [Migration("20211029160024_AlterProcStockMutationBB1")]
+    partial class AlterProcStockMutationBB1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
