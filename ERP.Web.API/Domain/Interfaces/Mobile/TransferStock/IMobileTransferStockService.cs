ca6a46bc933806d15cbd7e028a239eaab1eae261
Yacobus Sandi <yacobus.sandi@sahassa.co.id>
2021-11-05 10:37:26 +0700

    modif TransferStock, add new field on header

--- ERP.Web.API/Domain/Interfaces/Mobile/TransferStock/IMobileTransferStockService.cs
+++ ERP.Web.API/Domain/Interfaces/Mobile/TransferStock/IMobileTransferStockService.cs
@@ -11,9 +11,9 @@ namespace ERP.Web.API.Domain.Interfaces.Mobile.TransferStock
 {
     public interface IMobileTransferStockService
     {
-        IEnumerable<MobileTransferStockHeaderModel> getTranferStockHeader(DateTime? date,string search);
+        IEnumerable<MobileTransferStockHeaderModel> getTranferStockHeader(DateTime? date, string search, int userId);
         IEnumerable<MobileTransferStockDetailModel> getTransferStockDetail(string code);
-        IEnumerable<MobileTransferStockHeaderModel> getMobileTranferStockHeader(DateTime? date,string search);
+        IEnumerable<MobileTransferStockHeaderModel> getMobileTranferStockHeader(DateTime? date, string search, int userId);
         IEnumerable<MobileTransferStockDetailModel> getMobileTransferStockDetail(string code);
         SaveResult Insert(TransferStockRequestModel data, int userId);
 
