d17d3f885843e823a48a61162425927528894be9
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-12-08 13:23:08 +0700

    Fix validation multiple item

--- ERP.Web.API/Controllers/Sales/DirectInvoiceController.cs
+++ ERP.Web.API/Controllers/Sales/DirectInvoiceController.cs
@@ -136,8 +136,8 @@ namespace ERP.Web.API.Controllers.Sales
                 if (!data.ItemDetails.Any())
                     return (false, "Detail tidak boleh kosong.");
 
-                if (data.ItemDetails.GroupBy(x => new { x.ItemId }).Any(x => x.Count() > 1))
-                    return(false, "Terdapat kode pengiriman yang sama pada bagian detail.");
+                if (data.ItemDetails.GroupBy(x => new { x.ItemId, x.UnitId }).Any(x => x.Count() > 1))
+                    return (false, "Terdapat barang dengan satuan yang sama pada bagian detail.");
 
                 if (data.Memos.Any())
                 {
