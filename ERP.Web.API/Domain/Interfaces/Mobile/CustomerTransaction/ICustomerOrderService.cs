19824853de2b7a9b632be28d5b4ae8fb1f14aa51
Yacobus Sandi <yacobus.sandi@sahassa.co.id>
2021-12-09 11:37:39 +0700

    Insert Customer Order, Get CustomerOrderHeader, Get CustomerOrderDetail

--- ERP.Web.API/Domain/Interfaces/Mobile/CustomerTransaction/ICustomerOrderService.cs
+++ ERP.Web.API/Domain/Interfaces/Mobile/CustomerTransaction/ICustomerOrderService.cs
@@ -1,5 +1,10 @@
-﻿using ERP.Entity.General;
+﻿using ERP.Common;
+using ERP.Common.Models;
+using ERP.Entity.General;
+using ERP.Entity.MobileCustomer;
+using ERP.Web.API.Domain.Models.Mobile.CustomerOrder;
 using ERP.Web.API.Domain.Models.Mobile.CustomerPromotion;
+using ERP.Web.API.Domain.Models.Mobile.VisitOrder;
 
 namespace ERP.Web.API.Domain.Interfaces.Mobile.CustomerTransaction
 {
@@ -10,5 +15,10 @@ namespace ERP.Web.API.Domain.Interfaces.Mobile.CustomerTransaction
         IEnumerable<PromotionFreeGoodModel> GetPromotionFreeGood(int itemId, int itemCatId, decimal qty, int unitId, string custCode);
         IEnumerable<PromotionDiscountModel> GetPromotionInvoice(decimal amount, string custCode);
         bool GetParams();
+        DataSourceResult GetCustomerOrderHeader(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? date, string custCode);
+        IEnumerable<OrderCustomerDetailModel> GetCustomerOrderDetail(string orderId);
+        SaveResult InsertOrderCustomer(MobileOrderHeader header, IEnumerable<VisitOrderDetailRequest> details);
+
+
     }
 }
