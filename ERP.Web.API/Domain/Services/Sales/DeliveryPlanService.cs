34c574c6bbc1f900f9f93f70594f7b74abf25468
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-12-17 17:22:59 +0700

    Fix filter DO/DI in dlv plan

--- ERP.Web.API/Domain/Services/Sales/DeliveryPlanService.cs
+++ ERP.Web.API/Domain/Services/Sales/DeliveryPlanService.cs
@@ -12,7 +12,7 @@ namespace ERP.Web.API.Domain.Services.Sales
     public class DeliveryPlanService : GeneralService<DeliveryPlanHeader>, IDeliveryPlanService
     {
         public DeliveryPlanService(TenantContext db)
-            :base(db)
+            : base(db)
         {
 
         }
@@ -48,44 +48,27 @@ namespace ERP.Web.API.Domain.Services.Sales
 
         public DataSourceResult GetAllTransaction(string warehouseCode, IEnumerable<Filter> filter)
         {
-            var data = (from dt in Db.VwSalesDeliveryHeaders
-                        where dt.WarehouseCode == warehouseCode 
-                        && !new[] { "V", "INV" }.Contains(dt.Mark) 
-                        && !(from ddp in Db.DeliveryPlanDetails
-                             join dp in Db.DeliveryPlanHeaders on ddp.Code equals dp.Code
-                             where dp.Mark != "V" && ddp.FailedSendAll
-                             select ddp.TransCode).Contains(dt.Code)
+            var data = from dt in Db.VwSalesDeliveryHeaders
+                       where dt.WarehouseCode == warehouseCode
+                       && dt.Mark != "V" && (!(from ddp in Db.DeliveryPlanDetails
+                                               join dp in Db.DeliveryPlanHeaders on ddp.Code equals dp.Code
+                                               where dp.Mark != "V"
+                                               select ddp.TransCode).Contains(dt.Code)
+                        || (from ddp in Db.DeliveryPlanDetails
+                            join dp in Db.DeliveryPlanHeaders on ddp.Code equals dp.Code
+                            where dp.Mark != "V" && ddp.FailedSendAll
+                            select ddp.TransCode).Contains(dt.Code))
                         select new
                         {
                             dt.Code,
                             SoCode = dt.TransCode,
                             dt.Date,
                             dt.Mark,
-                            Type = "Surat Jalan",
+                            Type = dt.FromDirectInvoice ? "Penjualan Langsung" : "Surat Jalan",
                             dt.CustName,
                             dt.CustAddress,
                             dt.CustArea
-                        }
-                        ).Union(
-                        from dt in Db.VwSalesInvoiceHeaders
-                        where dt.FromDirectInvoice == true 
-                        && !new[] { "V", "INV" }.Contains(dt.Mark)
-                        && !(from ddp in Db.DeliveryPlanDetails
-                             join dp in Db.DeliveryPlanHeaders on ddp.Code equals dp.Code
-                             where dp.Mark != "V" && ddp.FailedSendAll
-                             select ddp.TransCode).Contains(dt.Code)
-                        select new
-                        {
-                            dt.Code,
-                            dt.SoCode,
-                            dt.Date,
-                            dt.Mark,
-                            Type = "Penjualan Langsung",
-                            dt.CustName,
-                            dt.CustAddress,
-                            dt.CustArea
-                        }
-                        );
+                        };
 
             return data.AsQueryable().ToDataSourceResult(0, data.Count(), filter, null);
         }
