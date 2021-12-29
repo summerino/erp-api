59bf93340a63f57fdffdb7574e2656e8d8761f44
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2021-12-29 17:42:57 +0700

    Adding Report Purchase Receive BackEnd

---
+++ ERP.Entity/Purchase/PurchaseReceiveReport.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Entity.Purchase
+{
+    public class ReportByRCV
+    {
+        public DateTime? Date { get; set; }
+
+        public string Code { get; set; }
+
+        public int SrcTrans { get; set; }
+
+        public string TransCode { get; set; }
+
+        public string RefNo { get; set; }
+
+        public string SupCode { get; set; }
+
+        public string SupName { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public decimal Disc { get; set; }
+
+        public decimal Dpp { get; set; }
+
+        public decimal TaxAmount { get; set; }
+
+        public decimal Total { get; set; }
+    }
+
+    public class ReportByDetailRCV
+    {
+        public DateTime? Date { get; set; }
+
+        public string Code { get; set; }
+
+        public int SrcTrans { get; set; }
+
+        public string TransCode { get; set; }
+
+        public string RefNo { get; set; }
+
+        public string SupCode { get; set; }
+
+        public string SupName { get; set; }
+
+        public string ItemInitial { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string WarehouseName { get; set; }
+
+        public decimal Qty { get; set; }
+
+        public string Unit { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public decimal Disc { get; set; }
+
+        public decimal DiscHeader { get; set; }
+
+        public decimal Dpp { get; set; }
+
+        public decimal TaxAmount { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
---
+++ ERP.Web.API/Domain/Interfaces/Purchase/IPurchaseReceiveReportService.cs
@@ -0,0 +1,11 @@
+﻿using ERP.Common.Models;
+
+namespace ERP.Web.API.Domain.Interfaces.Purchase
+{
+    public interface IPurchaseReceiveReportService
+    {
+        DataSourceResult GetData(int type, int? srcTrans, string startDate, string endDate,
+           string supCode, string status, int? itemId,
+           string code, bool isDetail);
+    }
+}
