e13a8622f7e8d8acddd4a9e1dbbdc84ec87bb9ed
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-02-14 11:50:22 +0700

    Adding Input Active Transaction BackEnd

---
+++ ERP.Entity/General/ActiveTransaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Entity.General
+{
+    public class ActiveTransactionGetDataRequest
+    {
+        public string Code { get; set; }
+
+        public string SrcName { get; set; }
+
+        public string Src { get; set; }
+
+    }
+}
