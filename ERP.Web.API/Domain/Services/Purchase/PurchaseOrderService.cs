15e667004abf8ac446023cbca29ae11c1cb19fb8
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-02-24 15:52:06 +0700

    Fix final discount header percent calculation

--- ERP.Web.API/Domain/Services/Purchase/PurchaseOrderService.cs
+++ ERP.Web.API/Domain/Services/Purchase/PurchaseOrderService.cs
@@ -123,15 +123,16 @@ namespace ERP.Web.API.Domain.Services.Purchase
                 {
                     var taxData = taxes.FirstOrDefault(x => x.Id == item.TaxId);
                     var discHeaderProrate = 0m;
+                    var sumDetail = data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty);
                     if (data.FinalDiscPercent > 0)
                     {
-                        var amountPercent = (data.IncludeTax ? data.SubTotal : data.Dpp) * (data.FinalDiscPercent / 100);
-                        discHeaderProrate = (amountPercent / data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty)) * (item.Qty * (item.UnitPrice - item.Disc));
+                        var amountPercent = sumDetail * (data.FinalDiscPercent / 100);
+                        discHeaderProrate = amountPercent / sumDetail * (item.Qty * (item.UnitPrice - item.Disc));
                         discHeaderProrate /= item.Qty;
                     }
                     else if (data.FinalDisc > 0)
                     {
-                        discHeaderProrate = (data.FinalDisc / data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty)) * (item.Qty * (item.UnitPrice - item.Disc));
+                        discHeaderProrate = data.FinalDisc / sumDetail * (item.Qty * (item.UnitPrice - item.Disc));
                         discHeaderProrate /= item.Qty;
                     }
 
@@ -468,15 +469,16 @@ namespace ERP.Web.API.Domain.Services.Purchase
                 {
                     var taxData = taxes.FirstOrDefault(x => x.Id == item.TaxId);
                     var discHeaderProrate = 0m;
+                    var sumDetail = data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty);
                     if (data.FinalDiscPercent > 0)
                     {
-                        var amountPercent = (data.IncludeTax ? data.SubTotal : data.Dpp) * (data.FinalDiscPercent / 100);
-                        discHeaderProrate = (amountPercent / data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty)) * (item.Qty * (item.UnitPrice - item.Disc));
+                        var amountPercent = sumDetail * (data.FinalDiscPercent / 100);
+                        discHeaderProrate = amountPercent / sumDetail * (item.Qty * (item.UnitPrice - item.Disc));
                         discHeaderProrate /= item.Qty;
                     }
                     else if (data.FinalDisc > 0)
                     {
-                        discHeaderProrate = (data.FinalDisc / data.ItemDetails.Sum(x => (x.UnitPrice - x.Disc) * x.Qty)) * (item.Qty * (item.UnitPrice - item.Disc));
+                        discHeaderProrate = data.FinalDisc / sumDetail * (item.Qty * (item.UnitPrice - item.Disc));
                         discHeaderProrate /= item.Qty;
                     }
 
