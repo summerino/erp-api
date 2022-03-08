7d859bb7a4fd80277316ce20826b36ed950d33d9
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-03-08 14:08:25 +0700

    Fix DI Credit limit validation

--- ERP.Web.API/Domain/Services/Sales/DirectInvoiceService.cs
+++ ERP.Web.API/Domain/Services/Sales/DirectInvoiceService.cs
@@ -1853,7 +1853,7 @@ public class DirectInvoiceService : GeneralService<SalesInvoiceHeader>, IDirectI
     }
 
     #region Credit Used - Limit
-    private bool CheckCreditLimit(string custCode, decimal total) => Db.Customers.Any(c => c.Code.Equals(custCode) && (c.CreditLimit - c.CreditUsed) >= total);
+    private bool CheckCreditLimit(string custCode, decimal total) => Db.Customers.Any(c => c.Code.Equals(custCode) && (c.CreditLimit <= 0 || (c.CreditLimit - c.CreditUsed) >= total));
     private void RestoreCreditUsed(string transCode, string custCode)
     {
         var prevAmount = Db.SalesOrderHeaders.AsNoTracking().FirstOrDefault(x => x.Code.Equals(transCode))?.Total;
