57fdbf9a5bdfe3618b1fbbc2598b03c79e1a34e9
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-03-11 16:38:54 +0700

    Add validation used/paid amount in beginning balance

--- ERP.Web.API/Domain/Services/Accounting/BeginningBalanceAccountPayableService.cs
+++ ERP.Web.API/Domain/Services/Accounting/BeginningBalanceAccountPayableService.cs
@@ -71,6 +71,12 @@ namespace ERP.Web.API.Domain.Services.Accounting
                 result.Message = "Kode sudah terdaftar. Tolong gunakan kode lain.";
                 return result;
             }
+
+            if (data.PaidAmount > data.Amount)
+            {
+                result.Message = "Nilai tidak boleh lebih kecil dari Nilai yang terbayarkan.";
+                return result;
+            }
             
             // Update data
             Db.BeginningBalanceAPs.Update(data);
