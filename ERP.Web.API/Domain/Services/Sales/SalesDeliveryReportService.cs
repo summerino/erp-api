e5b88ef4601ff371c92bc508165261a7fbba065a
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-05-31 09:20:58 +0700

    Remove Save & Send,Invoice process in SO if over limit

--- ERP.Web.API/Domain/Services/Sales/SalesDeliveryReportService.cs
+++ ERP.Web.API/Domain/Services/Sales/SalesDeliveryReportService.cs
@@ -24,13 +24,12 @@ public class SalesDeliveryReportService : ISalesDeliveryReportService
                             CASE do.Mark
 	                            WHEN 'A' THEN 'Aktif'
 	                            WHEN 'V' THEN 'Void'
-	                            WHEN 'INV' THEN 'Difakturkan'
-                                WHEN 'OL' THEN 'Kredit Melebihi Batas' END AS [Status]
+	                            WHEN 'INV' THEN 'Difakturkan' END AS [Status]
                             FROM Sales.vwSalesDeliveryHeader do
                             LEFT JOIN Sales.vwSalesDeliveryDetail do_d ON do_d.Code = do.Code
                             LEFT JOIN Inventory.Warehouse wh ON wh.Code = do.WarehouseCode
                             WHERE do.FromDirectInvoice = 0" +
-                                                (string.IsNullOrEmpty(status) ? "" : status.Replace("'", "''").Equals("NV") ? " AND do.Mark NOT IN ('V', 'OL)" : $" AND do.Mark = '{status.Replace("'", "''")}'") +
+                                                (string.IsNullOrEmpty(status) ? " AND do.Mark <> 'OL'" : status.Replace("'", "''").Equals("NV") ? " AND do.Mark NOT IN ('V', 'OL)" : $" AND do.Mark = '{status.Replace("'", "''")}'") +
                                                 " GROUP BY do.[Date], do.Code, do.CustCode, do.CustName, do.SrcTrans, do.TransCode, wh.[Name], do.Mark").ToList();
 
         var doDetailData = _db.ReportByDetailDOs.FromSqlRaw(@"SELECT do.[Date], do.Code, do.CustCode, do.CustName,
