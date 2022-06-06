972e568146f1bac5803c30f075a967cab1110e66
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2022-06-06 13:42:45 +0700

    fixing code

--- ERP.Web.API/Controllers/Finance/CashBankController.cs
+++ ERP.Web.API/Controllers/Finance/CashBankController.cs
@@ -6,12 +6,12 @@ using ERP.Entity;
 using ERP.Web.API.Domain.Interfaces.Accounting;
 using ERP.Web.API.Domain.Interfaces.Auth;
 using ERP.Web.API.Domain.Interfaces.Finance;
+using ERP.Web.API.Domain.Interfaces.General;
 using ERP.Web.API.Domain.Interfaces.SystemManagement;
 using ERP.Web.API.Domain.Models.Finance;
 using ERP.Web.API.Model;
 using ERP.Web.API.Model.Finance;
 using Newtonsoft.Json;
-using ERP.Web.API.Domain.Interfaces.General;
 
 namespace ERP.Web.API.Controllers.Finance;
 
@@ -25,6 +25,7 @@ public class CashBankController : ControllerBase
     private readonly IClaimService _claim;
     private readonly IAuthService _auth;
     private readonly IActiveTransactionService _activeTrans;
+
     private const int MenuId = (int)Menu.CashBank;
 
     public CashBankController(ICashBankService cb, IClosingMonthService closingMonth,
@@ -45,7 +46,7 @@ public class CashBankController : ControllerBase
         {
             var filter = JsonConvert.DeserializeObject<List<Filter>>(!string.IsNullOrWhiteSpace(filters) ? filters : "[]");
 
-            filter.Add(new Filter { Field = "createdBy", Operator = "eq", Keyword = (object)_claim.UserId });
+            filter.Add(new Filter { Field = "CreatedBy", Operator = "eq", Keyword = (object)_claim.UserId });
 
             filters = JsonConvert.SerializeObject(filter);
         }
