b7fed2778d21e490e1cc8a47e20b829f64a98f9b
Rifqi Ramadhani Muhammad <Rifqi.Ramadhani@sahassa.co.id>
2022-06-29 11:04:48 +0700

    Fix Transaction History Services - remove startDate and endDate props in Transaction Detail Sub Group Summary Model - add totalReal and totalMobile props from Sales Invoice and Mobile Order - add userId parameter - add mark filter - add sales invoice in sub group product and sub group summary, data obtained from mobile order and sales invoice (only group product and group summary)
    Deployed to test : #9157, #9158, #9164, #9171

--- ERP.Web.API/Domain/Interfaces/Mobile/TransactionHistory/ITransactionHistoryService.cs
+++ ERP.Web.API/Domain/Interfaces/Mobile/TransactionHistory/ITransactionHistoryService.cs
@@ -6,19 +6,19 @@ namespace ERP.Web.API.Domain.Interfaces.Mobile.TransactionHistory;
 
 public interface ITransactionHistoryService
 {
-    DataSourceResult GetDataByCustomer(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, string search);
-    DataSourceResult GetDataByDate(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate);
-    DataSourceResult GetDataByProduct(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, string search);
+    DataSourceResult GetDataByCustomer(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, string search, int userId);
+    DataSourceResult GetDataByDate(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, int userId);
+    DataSourceResult GetDataByProduct(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, string search, int userId);
     IEnumerable<TransactionHistoryByUnitProduct> GetDataByUnitProduct(int filterUnit, DateTime? startDate, DateTime? endDate, int userId);
-    DataSourceResult GetItemDetail(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, string search);
-    DataSourceResult GetCustomerDetail(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, string search);
+    DataSourceResult GetItemDetail(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, string search, int userId);
+    DataSourceResult GetCustomerDetail(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, string search, int userId);
     DataSourceResult GetDataCumulative(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, int year, string custCode, int userId);
     DataSourceResult GetDataByLog(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, string custCode, int userId);
     IEnumerable<ItemSubGroupModel> GetSubGroup();
     IEnumerable<ItemGroup> GetItemGroup();
     IEnumerable<ItemGroupSubGroup> GetItemSubGroup(int groupId);
     DataSourceResult GetDataBySubGroup(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? startDate, DateTime? endDate, int groupId, string subGroup, int userId);
-    DataSourceResult GetItemBySubGroup(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime date, int groupId, string subGroup);
+    DataSourceResult GetItemBySubGroup(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime date, int groupId, string subGroup, int userId);
     DataSourceResult GetDataBySubGroupSummary(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? filterStartDate, DateTime? filterEndDate, int? groupId, int? subGroupId, int userId);
     DataSourceResult GetDataDetailBySubGroupSummary(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? filterStartDate, DateTime? filterEndDate, int filterGroupId, int groupId, int subGroupId, int userId);
     DataSourceResult GetDataItemBySubGroupSummary(int skip, int take, IEnumerable<Filter> filter, IEnumerable<Sort> sort, DateTime? filterStartDate, DateTime? filterEndDate, string detailSubGroup, int userId);
