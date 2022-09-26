1deb55848728a22f1e36a82798148f32e37c84dc
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2022-09-26 10:11:45 +0700

    add transfer persediaan validation date

--- ERP.Web.API/Domain/Services/Inventory/TransferStockService.cs
+++ ERP.Web.API/Domain/Services/Inventory/TransferStockService.cs
@@ -59,6 +59,12 @@ public class TransferStockService : GeneralService<TransferStockHeader>, ITransf
         using var transaction = Db.Database.BeginTransaction();
         try
         {
+            if (data.Type == "IN" && Db.TransferStockHeaders.FirstOrDefault(x => x.Code == data.OriginTransferCode).Date > data.Date)
+            {
+                result.Message = "Tanggal transfer persediaan masuk tidak boleh lebih kecil dari tanggal transfer persediaan keluar.";
+                return result;
+            }
+
             // Get new code
             var newCode = GetNewCode("TS_NUM_FMT", data.Date);
 
@@ -184,6 +190,12 @@ public class TransferStockService : GeneralService<TransferStockHeader>, ITransf
             if (Db.TransferStockHeaders.Any(x => x.Code == data.Code && x.Mark == "V"))
             {
                 result.Message = "Data transfer persediaan tidak bisa diubah karena sudah ditandai sebagai void.";
+                return result;
+            }
+
+            if (data.Type == "IN" && Db.TransferStockHeaders.FirstOrDefault(x => x.Code == data.OriginTransferCode).Date > data.Date)
+            {
+                result.Message = "Tanggal transfer persediaan masuk tidak boleh lebih kecil dari tanggal transfer persediaan keluar.";
                 return result;
             }
 
