4b12a5842d1dc6b1de3ee978f35578bd116f2431
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2022-10-21 14:55:51 +0700

    update decimal precision & scale

--- ERP.Entity/Purchase/PurchaseReturn.cs
+++ ERP.Entity/Purchase/PurchaseReturn.cs
@@ -37,29 +37,29 @@ public class PurchaseReturnHeader : BaseEntityWithMarkApprovedAndViewed
     [Precision(18, 2)]
     public decimal Rate { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal ShipmentFee { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal HandlingFee { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal SubTotal { get; set; }
         
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal FinalDisc { get; set; }
 
     public bool NoTax { get; set; }
 
     public bool IncludeTax { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal TaxAmount { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal ExemptTaxAmount { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal Total { get; set; }
 
     [Column("DPP", TypeName = "decimal(18, 2)")]
@@ -97,32 +97,32 @@ public class VwPurchaseReturnHeader : BaseEntityWithMarkApprovedAndViewed
     [Precision(18, 2)]
     public decimal Rate { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal ShipmentFee { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal HandlingFee { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal SubTotal { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal FinalDisc { get; set; }
 
     public bool NoTax { get; set; }
 
     public bool IncludeTax { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal TaxAmount { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal ExemptTaxAmount { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal Total { get; set; }
 
-    [Precision(18, 2)]
+    [Precision(19, 6)]
     public decimal Dpp { get; set; }
 
     public string TaxInvoiceNo { get; set; }
