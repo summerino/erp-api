d3716483e28863e2c50860e17d70381ee6e03b9f
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2023-01-04 12:39:17 +0700

    missing commit prevent conccurency update when calculate hpp

--- ERP.Web.API/Domain/Services/Accounting/JournalService.cs
+++ ERP.Web.API/Domain/Services/Accounting/JournalService.cs
@@ -4321,30 +4321,33 @@ public class JournalService : IJournalService
             }
 
             var saved = false;
-            try
+            while (!saved)
             {
-                // Save HPP to DB
-                await db.SaveChangesAsync(cancellationToken);
-                saved = true;
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                foreach (var entry in ex.Entries)
+                try
                 {
-                    var proposedValues = entry.CurrentValues;
-                    var databaseValues = await entry.GetDatabaseValuesAsync(cancellationToken);
+                    // Save HPP to DB
+                    await db.SaveChangesAsync(cancellationToken);
+                    saved = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    foreach (var entry in ex.Entries)
+                    {
+                        var proposedValues = entry.CurrentValues;
+                        var databaseValues = await entry.GetDatabaseValuesAsync(cancellationToken);
                     
-                    // foreach (var property in proposedValues.Properties)
-                    // {
-                    //     var proposedValue = proposedValues[property];
-                    //     var databaseValue = databaseValues[property];
-                    //
-                    //     // TODO: decide which value should be written to database
-                    //     // proposedValues[property] = <value to be saved>;
-                    // }
+                        // foreach (var property in proposedValues.Properties)
+                        // {
+                        //     var proposedValue = proposedValues[property];
+                        //     var databaseValue = databaseValues[property];
+                        //
+                        //     // TODO: decide which value should be written to database
+                        //     // proposedValues[property] = <value to be saved>;
+                        // }
                     
-                    // Refresh original values to bypass next concurrency check
-                    entry.OriginalValues.SetValues(databaseValues);
+                        // Refresh original values to bypass next concurrency check
+                        entry.OriginalValues.SetValues(databaseValues);
+                    }
                 }
             }
         }
