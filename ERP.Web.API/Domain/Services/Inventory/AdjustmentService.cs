d30dbd4bf16c56e7761a93ec95d5ae14dd058efc
Galang Bilal <galang.bilal@sahassa.co.id>
2023-03-20 10:04:23 +0700

    Fix adjustment void typo

--- ERP.Web.API/Domain/Services/Inventory/AdjustmentService.cs
+++ ERP.Web.API/Domain/Services/Inventory/AdjustmentService.cs
@@ -387,7 +387,7 @@ public class AdjustmentService : GeneralService<AdjustmentHeader>, IAdjustmentSe
                 foreach (var itemGroup in groupedDetail)
                 {
                     var whQtyData = Db.VwWarehouseQuantities.FirstOrDefault(x => x.WarehouseCode == data.WarehouseCode && x.ItemId == itemGroup.ItemId);
-                    if ((whQtyData.QtyOnHand + itemGroup.Qty) < 0)
+                    if ((whQtyData.QtyOnHand - itemGroup.Qty) < 0)
                     {
                         result.Message = $"Data penyesuaian tidak bisa divoid karena terdapat barang pada gudang {whQtyData.WarehouseInitial} qty tersedia akan menjadi minus.";
                         return result;
