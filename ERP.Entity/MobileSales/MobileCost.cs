c8178738e3995296ac4bffe028f021530b89a71a
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2023-06-06 08:56:18 +0700

    revert MobileCost entity changes

--- ERP.Entity/MobileSales/MobileCost.cs
+++ ERP.Entity/MobileSales/MobileCost.cs
@@ -82,9 +82,6 @@ public class MobileCostDetail
 
     [Precision(18, 2)]
     public decimal Amount { get; set; }
-
-    [StringLength(256)]
-    public string Notes { get; set; }
 }
 
 public class VwMobileCostDetail
@@ -100,9 +97,6 @@ public class VwMobileCostDetail
     [Precision(18, 2)]
     public decimal Amount { get; set; }
 
-    public string Notes { get; set; }
-
-    
     public string CoaName { get; set; }
 }
 
