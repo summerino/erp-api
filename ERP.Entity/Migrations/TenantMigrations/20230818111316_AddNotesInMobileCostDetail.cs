685a470e5747f7c271020255373d30335ae50631
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2023-08-18 18:26:01 +0700

    adding migrations for column notes in MobileCostDetail

---
+++ ERP.Entity/Migrations/TenantMigrations/20230818111316_AddNotesInMobileCostDetail.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Entity.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class AddNotesInMobileCostDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                schema: "MobileSales",
+                table: "MobileCostDetail",
+                type: "varchar(256)",
+                unicode: false,
+                maxLength: 256,
+                nullable: true);
+
+            // Refresh view MobileSales.vwMobileCostDetail
+            var sql = @"exec sp_refreshview 'MobileSales.vwMobileCostDetail'";
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                schema: "MobileSales",
+                table: "MobileCostDetail");
+        }
+    }
+}
