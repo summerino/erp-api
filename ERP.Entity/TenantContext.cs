632c5387c12d51184e79dd8c347cb978ab52a61d
Galang Bilal Farchan <galang.bilal@sahassa.co.id>
2023-10-02 18:10:43 +0700

    Improve cash bank report query #14789

--- ERP.Entity/TenantContext.cs
+++ ERP.Entity/TenantContext.cs
@@ -93,6 +93,10 @@ public class TenantContext : DbContext
     public DbSet<OutstandingChequeReport> OutstandingChequeReports { get; set; }
     public DbSet<CashFlowReportAll> CashFlowReportAlls { get; set; }
     public DbSet<CashFlowReportByCOA> CashFlowReportByCOAs { get; set; }
+    public DbSet<ReportByAllAccount> ReportByAllAccounts { get; set; }
+    public DbSet<ReportByAccount> ReportByAccounts { get; set; }
+    public DbSet<ReportByAccountDetail> ReportByAccountDetails { get; set; }
+
 
     // General entities
     public DbSet<VwApproval> VwApprovals { get; set; }
@@ -757,6 +761,18 @@ public class TenantContext : DbContext
         modelBuilder.Entity<CashFlowReportByCOA>()
             .HasNoKey()
             .ToTable("CashFlowReportByCOA", t => t.ExcludeFromMigrations());
+
+        modelBuilder.Entity<ReportByAllAccount>()
+            .HasNoKey()
+            .ToTable("ReportByAllAccount", t => t.ExcludeFromMigrations());
+
+        modelBuilder.Entity<ReportByAccount>()
+            .HasNoKey()
+            .ToTable("ReportByAccount", t => t.ExcludeFromMigrations());
+
+        modelBuilder.Entity<ReportByAccountDetail>()
+            .HasNoKey()
+            .ToTable("ReportByAccountDetail", t => t.ExcludeFromMigrations());
 
         // General entities
         // Approval model
