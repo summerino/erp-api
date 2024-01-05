a646c9e054e2215608082d47e8229d0e5e7f936c
Rudy Wang <rudy.sepbroto@sahassa.co.id>
2024-01-05 15:47:30 +0700

    adding migration alter column amount at journal

---
+++ ERP.Entity/Migrations/TenantMigrations/20240105084639_AlterColumnAmountAtJurnal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Entity.Migrations.TenantMigrations
+{
+    /// <inheritdoc />
+    public partial class AlterColumnAmountAtJurnal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                schema: "Accounting",
+                table: "Journal",
+                type: "decimal(22,9)",
+                precision: 22,
+                scale: 9,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(19,6)",
+                oldPrecision: 19,
+                oldScale: 6);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                schema: "Accounting",
+                table: "Journal",
+                type: "decimal(19,6)",
+                precision: 19,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(22,9)",
+                oldPrecision: 22,
+                oldScale: 9);
+        }
+    }
+}
